fe8066f405fecdb9c7ed5dd7dd34532c23eaf538
mzun <mzun@mzun-EX625.(none)>
2014-01-22 12:19:10 +0100

    Add qt file

---
+++ main.cpp
@@ -0,0 +1,92 @@
+#include <QCoreApplication>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <QElapsedTimer>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+Mat src, src_gray;
+Mat dst, detected_edges;
+
+int edgeThresh = 1;
+int lowThreshold;
+int const max_lowThreshold = 100;
+int ratio = 3;
+int kernel_size = 3;
+char* window_name = "Edge Map";
+
+/**
+ * @function CannyThreshold
+ * @brief Trackbar callback - Canny thresholds input with a ratio 1:3
+ */
+void CannyThreshold(int, void*)
+{
+    QElapsedTimer *timer = new QElapsedTimer();
+    timer->restart();
+  /// Reduce noise with a kernel 3x3
+  blur( src_gray, detected_edges, Size(3,3) );
+
+  /// Canny detector
+  Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
+
+  /// Using Canny's output as a mask, we display our result
+  dst = Scalar::all(0);
+
+  src.copyTo( dst, detected_edges);
+  std::cout<<"Czas detekcji krawędzi: "<<timer->nsecsElapsed()<<endl;
+  timer->restart();
+  delete timer;
+  imshow( window_name, dst );
+ }
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    QElapsedTimer *timer = new QElapsedTimer();
+
+//    cv::Mat obraz = cv::imread("/home/mzun/Pictures/pic.jpeg");
+
+//    cv::imshow("af",obraz);
+    /// Load an image
+
+    timer->start();
+    src = imread("/home/mzun/Pictures/pic.jpeg");
+    std::cout<<"Czas wczytywania pliku: "<<timer->nsecsElapsed()<<endl;
+    timer->restart();
+
+    if( !src.data )
+    { return -1; }
+
+    /// Create a matrix of the same type and size as src (for dst)
+    dst.create( src.size(), src.type() );
+    std::cout<<"Czas powielenia obrazu: "<<timer->nsecsElapsed()<<endl;
+    timer->restart();
+
+    /// Convert the image to grayscale
+    cvtColor( src, src_gray, CV_BGR2GRAY );
+    std::cout<<"Czas konwersji do skali szarości: "<<timer->nsecsElapsed()<<endl;
+    timer->restart();
+
+    /// Create a window
+    namedWindow( window_name, CV_WINDOW_AUTOSIZE );
+
+    /// Create a Trackbar for user to enter threshold
+    createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold );
+
+    /// Show the image
+    CannyThreshold(0, 0);
+
+    /// Wait until user exit program by pressing a key
+    waitKey(0);
+
+    delete timer;
+
+    return 0;
+
+
+
+    return a.exec();
+}
